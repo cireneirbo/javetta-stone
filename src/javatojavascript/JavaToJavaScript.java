8cde037ecb2b7e8dbbf38733da258f4f55d6c005
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2020-07-23 23:07:21 -0400

    added some horrific looking logic

--- src/javatojavascript/JavaToJavaScript.java
+++ src/javatojavascript/JavaToJavaScript.java
@@ -5,10 +5,36 @@ public abstract class JavaToJavaScript {
 
     public static String translateJavaLineToJavaScript(String line) {
 
+        //String javaScriptTranslation = "";
+
         if (line.equals("public static void main(String[] args) {")){
             return "W";
+        } if (line.equals("String stringQuestion = \"What is the difference between\\n\" +")){
+            return "W";
+        } if (line.equals("\"a ' and a \\\"?  Or between a \\\" and a \\\\\\\"?\";")){
+            return "W";
+        } if (line.equals("String stringAnswer = \"One is what we see when we're typing our program.\\n\" +")){
+            return "W";
+        } if (line.equals("\"The other is what appears on the \\\"console.\\\"\";")){
+            return "W";
+        } if (line.equals("Scanner input;")){
+            return "W";
+        } if (line.equals("input = new Scanner(System.in);")){
+            return "W";
+        } if (line.equals("System.out.println(stringQuestion);")){
+            return "W";
+        } if (line.equals("String userAnswer = input.next();")){
+            return "W";
+        } if (line.equals("input.close();")){
+            return "W";
+        } if (line.equals("System.out.println(\"You said: \" + userAnswer);")){
+            return "W";
+        } if (line.equals("System.out.println(\"The answer: \" + stringAnswer);")){
+            return "W";
+        } if (line.equals("}")){
+            return "W";
         } else {
-            return "M";
+            return ""; //blank line
         }
 
 
